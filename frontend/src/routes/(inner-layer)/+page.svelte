9df078aa44e1cedf8c234b220359910c069280b9
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-01-29 17:39:10 -0300

    feature/criar página inicial

--- frontend/src/routes/(inner-layer)/+page.svelte
+++ frontend/src/routes/(inner-layer)/+page.svelte
@@ -1,4 +1,41 @@
 <script>
+	import Card from '$lib/components/card.svelte';
 </script>
 
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+<Card class="max-width-60">
+	<h1 class="text-center">Biblioteca do Cursinho A23</h1>
+	<div class="d-flex justify-content-center">
+		<img src="/logo.png" alt="Logo do Cursinho A23" class="logo" />
+	</div>
+	<p class="mt-3">
+		Seja bem-vind@ à biblioteca do Cursinho Popular Alicerce 23! Aqui você pode emprestar um dos
+		livros do nosso acervo.
+	</p>
+	<p>
+		Para procurar um livro para emprestar, clique em <a href="/emprestar" class="text-secondary"
+			>Buscar Livro</a
+		> na barra de navegação.
+	</p>
+	<p>
+		Para verificar seu histórico de empréstimos, clique em <a
+			href="/historico"
+			class="text-secondary">Histórico</a
+		> na barra de navegação.
+	</p>
+	<p>
+		Para renovar o livro que você está emprestando atualmente, clique em <a
+			href="/renovar"
+			class="text-secondary">Renovar</a
+		> na barra de navegação.
+	</p>
+</Card>
+
+<style>
+	:global(.max-width-60) {
+		max-width: 60%;
+	}
+
+	.logo {
+		width: 200px;
+	}
+</style>
