81f218f27547f410a5597e532ba483634209fa52
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-01-29 18:38:52 -0300

    feature/criar mock de página de busca de livro

---
+++ frontend/src/routes/(inner-layer)/buscar/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Card from '$lib/components/card.svelte';
+</script>
+
+<Card class="w-75">
+	<h1 class="text-center">Buscar livro</h1>
+	<div class="d-flex justify-content-center mt-5">
+		<form class="d-flex w-90" role="search">
+			<input
+				class="form-control me-2"
+				type="search"
+				placeholder="Pesquisar livro"
+				aria-label="Search"
+			/>
+			<button class="btn btn-outline-success" type="submit">Buscar</button>
+		</form>
+	</div>
+	<div class="w-90"></div>
+	<div class="d-flex justify-content-center mt-5 px-5">
+		<table class="table table-striped">
+			<thead>
+				<th>Nome</th>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						<details>
+							<summary> João e o Pé de Feijão </summary>
+							<ul>
+								<li>Gênero: aaaa</li>
+								<li>Exemplares disponíveis: 10</li>
+							</ul>
+							<button class="btn btn-dark">Emprestar</button>
+						</details>
+					</td>
+				</tr>
+				<tr
+					><td
+						><details>
+							<summary> Manifesto Comunista </summary>
+							<ul>
+								<li>Gênero: bbbb</li>
+								<li>Exemplares disponíveis: 5</li>
+							</ul>
+							<button class="btn btn-dark">Emprestar</button>
+						</details>
+					</td></tr
+				>
+			</tbody>
+		</table>
+	</div>
+</Card>
+
+<style>
+	.w-90 {
+		width: 90%;
+	}
+</style>
