05a24306dfa016c9feb22bb9756a0bd9d1606da5
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-02-08 18:25:08 -0300

    feature/fazer migration

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,11 +1,13 @@
 package database
 
 import (
+	m "biblioteca-a23/models"
 	"fmt"
+	"os"
+
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"os"
 )
 
 func ConnectToDatabase() (*gorm.DB, error) {
@@ -16,7 +18,7 @@ func ConnectToDatabase() (*gorm.DB, error) {
 	}
 
 	dsn := fmt.Sprintf(
-		"%s:%s@tcp(127.0.0.1:3306)/biblioteca",
+		"%s:%s@tcp(127.0.0.1:3306)/biblioteca?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("MYSQL_USER"),
 		os.Getenv("MYSQL_PASSWORD"),
 	)
@@ -24,4 +26,15 @@ func ConnectToDatabase() (*gorm.DB, error) {
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	return db, err
+}
+
+func MakeMigration(db *gorm.DB) {
+	db.AutoMigrate(
+		&m.User{},
+		&m.Reader{},
+		&m.Admin{},
+		&m.Book{},
+		&m.Copy{},
+		&m.Loan{},
+	)
 }
--- backend/main.go
+++ backend/main.go
@@ -1,42 +1,29 @@
 package main
 
 import (
-	"fmt"
-
 	"biblioteca-a23/database"
-	"biblioteca-a23/models"
+	//"biblioteca-a23/models"
+	//"fmt"
 	"github.com/go-playground/validator/v10"
-	"golang.org/x/crypto/bcrypt"
 )
 
 var validate *validator.Validate = validator.New(validator.WithRequiredStructEnabled())
 
 func main() {
-	// teste validator
-
-	user := models.User{
-		Name:     "Doglas",
-		Surname:  "Rocha",
-		Email:    "teste@teste.com",
-		Password: "12345678",
-	}
-
-	//fmt.Println(user)
-	fmt.Println(user.Validate(validate))
-
-	// teste hash
-	var teste string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-
-	hash, err := bcrypt.GenerateFromPassword([]byte(teste), bcrypt.MinCost)
-
-	if err != nil {
-		panic("aaaaa")
-	}
-
-	fmt.Print(string(hash))
 
 	// teste database
 	db, err := database.ConnectToDatabase()
 
-	fmt.Println(db.First(&user, 1))
+	if err != nil {
+		panic("erro na conexao")
+	}
+
+	database.MakeMigration(db)
+
+	// db.Create(&models.User{Name: "Doglas", Surname: "Rocha", Email: "doglas", Password: "rocha"})
+	/*var user models.User
+	db.First(&user)
+	fmt.Println(user)
+
+	db.Delete(&user)*/
 }
