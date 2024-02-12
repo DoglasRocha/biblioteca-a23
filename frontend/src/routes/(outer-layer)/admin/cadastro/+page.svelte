e1f8b9999b93c6a22c5876a98da2e937e4a90a8f
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-02-11 23:26:11 -0300

    feature/adicionar validacoes no cadastro de admins

--- frontend/src/routes/(outer-layer)/admin/cadastro/+page.svelte
+++ frontend/src/routes/(outer-layer)/admin/cadastro/+page.svelte
@@ -1,28 +1,59 @@
 <script>
 	import FormField from '$lib/components/form-field.svelte';
 	import Card from '$lib/components/card.svelte';
+	import BlankForm from '$lib/components/blank-form.svelte';
+	import { api } from '$lib/utils/api.js';
+	import { isValidUserInput } from '$lib/utils/helpers.js';
 
 	let userData = {
-		firstName: '',
-		lastName: '',
+		name: '',
+		surname: '',
 		email: '',
 		password: ''
 	};
 
-	let passwordConfirmation;
+	let isInvalid = {
+		name: false,
+		surname: false,
+		email: false,
+		password: false,
+		passwordConfirmation: false
+	};
+
+	let passwordConfirmation, errorFromServer;
+
+	const handleSubmit = async () => {
+		if (!isValidUserInput(isInvalid)) return;
+
+		if (userData.password != passwordConfirmation) {
+			isInvalid.passwordConfirmation = true;
+			return;
+		}
+
+		try {
+			let request = await api.passwordConfirmation('/admin/cadastro', userData);
+
+			if (request.status == 201) document.location.href = '/admin/login';
+		} catch (error) {
+			errorFromServer = error.response.data;
+		}
+	};
 </script>
 
 <Card>
 	<h1>Cadastro</h1>
-	<form>
+	<BlankForm>
 		<div class="d-flex">
 			<div class="me-1">
 				<FormField
 					name="first-name"
 					label="Nome"
 					placeholder="João"
-					bind:value={userData.firstName}
+					bind:value={userData.name}
 					type="text"
+					errorMsg="Seu nome deve possuir três ou mais letras"
+					validation={/[\w ]{3,50}/}
+					bind:isInvalid={isInvalid.name}
 					required
 				/>
 			</div>
@@ -31,8 +62,11 @@
 					name="last-name"
 					label="Sobrenome"
 					placeholder="Silva"
-					bind:value={userData.lastName}
+					bind:value={userData.surname}
 					type="text"
+					errorMsg="Seu sobrenome deve possuir três ou mais letras"
+					validation={/[\w ]{3,100}/}
+					bind:isInvalid={isInvalid.surname}
 					required
 				/>
 			</div>
@@ -43,6 +77,9 @@
 			placeholder="exemplo@email.com"
 			bind:value={userData.email}
 			type="email"
+			errorMsg="Email inválido"
+			validation={/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/}
+			bind:isInvalid={isInvalid.email}
 			required
 		/>
 		<FormField
@@ -50,25 +87,31 @@
 			label="Senha "
 			bind:value={userData.password}
 			type="password"
+			errorMsg="Sua senha deve possuir mais de oito dígitos"
+			validation={/.{8,}/}
+			bind:isInvalid={isInvalid.password}
 			required
 		/>
 		<FormField
-		name="password-confirmation"
-		label="Confirmação de senha"
-		bind:value={passwordConfirmation}
-		type="password"
-		required
+			name="password-confirmation"
+			label="Confirmação de senha"
+			bind:value={passwordConfirmation}
+			type="password"
+			errorMsg="As senhas não são iguais"
+			validation={/.*?/}
+			bind:isInvalid={isInvalid.passwordConfirmation}
+			required
 		/>
 		<div class="mt-3 d-flex justify-content-end">
 			<button
 				type="submit"
 				class="btn btn-primary"
 				on:click={() => {
-					document.location.href = '/login';
+					document.location.href = '/admin/login';
 				}}>Cadastrar</button
 			>
 		</div>
-	</form>
+	</BlankForm>
 </Card>
 
 <style>
--- frontend/src/routes/(outer-layer)/cadastro/+page.svelte
+++ frontend/src/routes/(outer-layer)/cadastro/+page.svelte
@@ -3,6 +3,7 @@
 	import BlankForm from '$lib/components/blank-form.svelte';
 	import Card from '../../../lib/components/card.svelte';
 	import { api } from '$lib/utils/api.js';
+	import { isValidUserInput } from '$lib/utils/helpers.js';
 
 	let userData = {
 		name: '',
@@ -25,18 +26,10 @@
 		passwordConfirmation: false
 	};
 
-	let passwordConfirmation;
-	let errorFromServer;
-
-	const isValidUserInput = () => {
-		for (let key in isInvalid) {
-			if (isInvalid[key]) return false;
-		}
-		return true;
-	};
+	let passwordConfirmation, errorFromServer;
 
 	const handleSubmit = async () => {
-		if (!isValidUserInput()) return;
+		if (!isValidUserInput(isInvalid)) return;
 
 		if (userData.password != passwordConfirmation) {
 			isInvalid.passwordConfirmation = true;
@@ -46,15 +39,14 @@
 		// go date format
 		userData.birthday = `${userData.birthday}T00:00:00Z`;
 
-		let request;
 		try {
-			request = await api.post('/cadastro', userData);
-			// workaround for html "required"
+			let request = await api.post('/cadastro', userData);
 
 			if (request.status == 201) document.location.href = '/login';
 		} catch (error) {
 			errorFromServer = error.response.data;
 		}
+		// workaround for html "required"
 		userData.birthday = userData.birthday.split('T')[0];
 	};
 </script>
