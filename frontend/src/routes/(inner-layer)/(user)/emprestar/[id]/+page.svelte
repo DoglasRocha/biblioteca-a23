031bd185c91ac98533d2137ca7c2fbab07a9d320
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-02-13 16:37:33 -0300

    feature/integrar com busca de usuario leitor no frontend

--- frontend/src/routes/(inner-layer)/(user)/emprestar/[id]/+page.svelte
+++ frontend/src/routes/(inner-layer)/(user)/emprestar/[id]/+page.svelte
@@ -1,22 +1,30 @@
 <script>
 	import Card from '$lib/components/card.svelte';
+
+	export let data;
+
+	let book = data.book,
+		error = data.error;
 </script>
 
 <Card class="w-75">
-	<h1 class="text-center">João e o Pé de Feijão</h1>
-	<ul>
-		<li>
-			<span class="fw-bold">Descrição: </span>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, atque. Quidem sit dolores
-				aliquid repellendus temporibus blanditiis ut eos culpa possimus quibusdam officiis cum
-				maxime, a reprehenderit aut laboriosam architecto!
-			</p>
-		</li>
-		<li><span class="fw-bold">Gênero: </span>aaaa</li>
-		<li><span class="fw-bold">Exemplares disponíveis: </span>5</li>
-	</ul>
-	<div class="d-flex justify-content-end">
-		<button class="btn btn-primary">Emprestar</button>
-	</div>
+	{#if error}
+		<h1>Erro!</h1>
+		<p>Não há livro com este identificador</p>
+	{:else}
+		<h1 class="text-center">{book.name}</h1>
+		<ul class="mt-3">
+			<li>
+				<span class="fw-bold">Descrição: </span>
+				<p>
+					{book.description}
+				</p>
+			</li>
+			<li><span class="fw-bold">Gênero: </span>{book.gender}</li>
+			<li><span class="fw-bold">Exemplares disponíveis: </span>{book.copies}</li>
+		</ul>
+		<div class="d-flex justify-content-end">
+			<button class="btn btn-primary">Emprestar</button>
+		</div>
+	{/if}
 </Card>
