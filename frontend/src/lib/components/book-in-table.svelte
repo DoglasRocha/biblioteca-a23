865522b6d74f60416711d7f8f62d832f0f3d4455
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-02-14 16:39:16 -0300

    fix/arrumar link do botao de deleção e implementar botão de delete

--- frontend/src/lib/components/book-in-table.svelte
+++ frontend/src/lib/components/book-in-table.svelte
@@ -8,6 +8,12 @@
 	<ul>
 		<li><span class="fw-bold">Gênero: </span>{book.gender}</li>
 		<li><span class="fw-bold">Exemplares disponíveis: </span>{book.copies}</li>
+		{#if admin}
+			<li>
+				<span class="fw-bold">Data da última atualização:</span>
+				{new Date(Date.parse(book.CreatedAt)).toLocaleDateString('pt-br')}
+			</li>
+		{/if}
 		<li>
 			<span class="fw-bold">Descrição: </span>
 			<p>{book.description}</p>
