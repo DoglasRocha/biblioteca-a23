dfa12df6453977ca80e43bae8623aa9121a28478
Larissa Rodrigues <larissarodrigues@alunos.utfpr.edu.br>
2024-02-19 22:00:19 -0300

    Apagando comentarios inuteis

--- frontend/src/routes/(inner-layer)/(admin)/admin/admins/+page.svelte
+++ frontend/src/routes/(inner-layer)/(admin)/admin/admins/+page.svelte
@@ -46,55 +46,11 @@
 			</tr>
 			</thead>
 			<tbody>
-								<!--
-
-				{#if error}
-					<tr>
-						<td> Não há livros!! </td>
-					</tr>
-				{:else}
-					{#each books as book}
-						<tr>
-							<td>
-								<BookInTable {book} admin={true}>
-									<a href={`/admin/livros/editar/${book.id}`} class="btn btn-secondary"> Editar </a>
-									<button
-										type="button"
-										class="btn btn-danger ms-1"
-										on:click={async () => {
-											let confirmation = confirm(
-												'Você tem certeza que deseja deletar esse livro e todas as suas cópias?'
-											);
-											if (confirmation) {
-												//await api.delete(`/admin/livros/deletar/${book.id}`);
-												document.location.href = document.location.href + '/';
-											}
-										}}>Deletar</button
-									>
-								</BookInTable>
-							</td>
-						</tr>
-						
-					{:else}
-						<tr>
-							<td> Não há livros!! </td>
-						</tr>
-					{/each}
-				{/if}
-				-->
 			</tbody>
 		</table>
 	</div>
-
-
-
-
-		
-
-
-	
-
 </Card>
+
 
 <style>
 </style>
