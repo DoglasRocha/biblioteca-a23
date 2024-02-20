0cc7e55324480c1bb418f42587fb139122d48b52
Larissa Rodrigues <larissarodrigues@alunos.utfpr.edu.br>
2024-02-19 21:53:59 -0300

    Tela de listagem de admin. Fixed#31

--- frontend/src/routes/(inner-layer)/(admin)/admin/admins/+page.svelte
+++ frontend/src/routes/(inner-layer)/(admin)/admin/admins/+page.svelte
@@ -2,4 +2,99 @@
 	import Card from '$lib/components/card.svelte';
 </script>
 
-<Card class="w-75"></Card>
+<Card class="w-75">
+	<h1 class="text-center">Administradores</h1>
+
+	<div class="d-flex justify-content-center mt-5 px-5">
+
+		<table class="table table-striped">
+			<thead>
+				<th>Nome</th>
+			<tr>
+				<td>
+					<details>
+						<summary>Admin1 Silva</summary>
+	
+					<p>Email : exemplo@email.com</p>
+					<p>Status: Liberado</p>	
+					<div>
+						<button 
+						type="button"
+						class="btn btn-danger ms-1"
+						>Revogar
+						</button>
+					</div> 
+				</td>
+
+			</tr>
+			<tr>
+				<td>
+					<details>
+						<summary>Admin2 Rocha</summary>
+	
+					<p>Email : exemplo2@email.com</p>
+					<p>Status: Não Liberado</p>	
+					<div>
+						<button 
+						type="button"
+						class="btn btn-success ms-1"
+						>Autorizar
+						</button>
+					</div>
+				</td>
+	
+			</tr>
+			</thead>
+			<tbody>
+								<!--
+
+				{#if error}
+					<tr>
+						<td> Não há livros!! </td>
+					</tr>
+				{:else}
+					{#each books as book}
+						<tr>
+							<td>
+								<BookInTable {book} admin={true}>
+									<a href={`/admin/livros/editar/${book.id}`} class="btn btn-secondary"> Editar </a>
+									<button
+										type="button"
+										class="btn btn-danger ms-1"
+										on:click={async () => {
+											let confirmation = confirm(
+												'Você tem certeza que deseja deletar esse livro e todas as suas cópias?'
+											);
+											if (confirmation) {
+												//await api.delete(`/admin/livros/deletar/${book.id}`);
+												document.location.href = document.location.href + '/';
+											}
+										}}>Deletar</button
+									>
+								</BookInTable>
+							</td>
+						</tr>
+						
+					{:else}
+						<tr>
+							<td> Não há livros!! </td>
+						</tr>
+					{/each}
+				{/if}
+				-->
+			</tbody>
+		</table>
+	</div>
+
+
+
+
+		
+
+
+	
+
+</Card>
+
+<style>
+</style>
