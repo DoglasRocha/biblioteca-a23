0323249a57e8d98b23a243ef078c26dc893ffa8b
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-02-21 18:28:24 -0300

    feature/implementar revogacao de acesso de admin

--- backend/controllers/admins.go
+++ backend/controllers/admins.go
@@ -85,9 +85,49 @@ func AuthorizeAdmin(w http.ResponseWriter, r *http.Request) {
 }
 
 func RevokeAdmin(w http.ResponseWriter, r *http.Request) {
-	// if admin_to_authorize.UserID == current_admin.UserID {
-	// 	w.WriteHeader(http.StatusNotAcceptable)
-	// 	fmt.Fprintln(w, "Um admin não pode se revogar")
-	// 	return
-	// }
+	var admin_to_revoke models.Admin
+	var current_admin models.Admin
+
+	status := is_admin_authenticated(w, r)
+	if status != http.StatusOK {
+		return
+	}
+
+	// get current admin
+	current_admin_id, err := get_id_from_request_cookie(w, r)
+	if err != nil {
+		return
+	}
+	err = database.DB.Where("user_id = ?", current_admin_id).Find(&current_admin).Error
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, "Erro ao encontrar admin corrente na base de dados")
+		return
+	}
+
+	// get admin to revoke
+	admin_to_revoke_id := mux.Vars(r)["admin_id"]
+	err = database.DB.Where("user_id = ?", admin_to_revoke_id).Find(&admin_to_revoke).Error
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, "Erro ao encontrar admin para autorizar na base de dados")
+		return
+	}
+
+	if admin_to_revoke.UserID == current_admin.UserID {
+		w.WriteHeader(http.StatusNotAcceptable)
+		fmt.Fprintln(w, "Um admin não pode se revogar")
+		return
+	}
+
+	admin_to_revoke.IsCleared = false
+	err = database.DB.Save(&admin_to_revoke).Error
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Erro ao autorizar administrador")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Admin revogado com sucesso")
 }
