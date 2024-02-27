02f19f366743bc6bdca3a0c438171bd74b5d8ed9
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-02-26 22:50:34 -0300

    feature/fazer mock de página de dados de usuário

---
+++ frontend/src/routes/(inner-layer)/(admin)/admin/usuario/[id]/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Card from '$lib/components/card.svelte';
+	import Bold from '$lib/components/bold.svelte';
+
+	export let data;
+
+	let readerData = data.readerData,
+		error = data.error;
+</script>
+
+<Card class="w-75">
+	<h1 class="text-center">Dados de Usuário</h1>
+	{#if readerData}
+		<ul>
+			<li>
+				<Bold>Nome:</Bold>
+			</li>
+			<li>
+				<Bold>Email:</Bold>
+			</li>
+			<li>
+				<Bold>Data de nascimento:</Bold>
+			</li>
+			<li>
+				<Bold>Endereço:</Bold>
+			</li>
+			<li>
+				<Bold>Número de telefone:</Bold>
+			</li>
+		</ul>
+	{:else}
+		<div class="text-danger">
+			<p>Ops, aconteceu um erro. Talvez isso ajude:</p>
+			<p>{error}</p>
+		</div>
+	{/if}
+</Card>
