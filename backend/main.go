4f6d770264e3da87c1abc41173900dc973ee1131
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-02-27 22:06:39 -0300

    feature/fazer logs no common.go

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"biblioteca-a23/database"
 	"biblioteca-a23/routes"
+	"log"
+	"log/slog"
+	"os"
 
 	"github.com/joho/godotenv"
 	//"biblioteca-a23/models"
@@ -10,8 +13,17 @@ import (
 )
 
 func main() {
+	// configura logger
+	f, err := os.OpenFile("logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer f.Close()
+	logger := slog.New(slog.NewJSONHandler(f, nil))
+	slog.SetDefault(logger)
+
 	// loads environment variables
-	err := godotenv.Load(".env")
+	err = godotenv.Load(".env")
 	if err != nil {
 		panic("Error loading .env file")
 	}
