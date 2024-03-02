7a44da921c1d63fe529910143978c97f30dfba25
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-03-02 12:17:40 -0300

    fix/adicionar dominio nas origens permitidas

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -30,7 +30,7 @@ func SetupRoutes() {
 		"Content-Language",
 		"Origin",
 	})
-	originsOk := handlers.AllowedOrigins([]string{"https://biblioteca-a23.vercel.app"})
+	originsOk := handlers.AllowedOrigins([]string{"https://biblioteca-a23.vercel.app", "https://biblioteca.a23cursinho.live"})
 	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"})
 	credentialsOk := handlers.AllowCredentials()
 
