ac94af19272c0d4d6414a7cd8c15440411b3bbe1
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-03-02 21:47:56 -0300

    debug

--- frontend/src/routes/(inner-layer)/(admin)/+layout.server.js
+++ frontend/src/routes/(inner-layer)/(admin)/+layout.server.js
@@ -3,7 +3,7 @@ import { api } from "$lib/utils/api.js";
 
 export async function load({ cookies }) {
 
-    console.log(cookies);
+    console.log('coookies', cookies);
     const accessToken = cookies.get("accessToken")
     console.log(accessToken)
 
