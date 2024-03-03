0ccc851d63dd252ca9f1284f204c76f28fa021e8
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-03-02 21:42:07 -0300

    debug/verificar cookies q vem

--- frontend/src/routes/(inner-layer)/(admin)/+layout.server.js
+++ frontend/src/routes/(inner-layer)/(admin)/+layout.server.js
@@ -3,6 +3,7 @@ import { api } from "$lib/utils/api.js";
 
 export async function load({ cookies }) {
 
+    console.log(cookies);
     const accessToken = cookies.get("accessToken")
     console.log(accessToken)
 
