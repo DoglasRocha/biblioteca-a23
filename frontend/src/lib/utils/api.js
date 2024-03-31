6b87b6e8334a9953f1a289c76cc47c83f1f5795a
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-03-31 16:54:23 -0300

    fix/front allowed origins

--- frontend/src/lib/utils/api.js
+++ frontend/src/lib/utils/api.js
@@ -4,8 +4,8 @@ import { env } from "$env/dynamic/public"
 export const api = axios.create({
     baseURL: env.PUBLIC_API_URL,
     withCredentials: true,
-    // headers: {
-    //     //"Access-Control-Allow-Origin": "*",
-    //     "Access-Control-Allow-Credentials": true,
-    //   },
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        //"Access-Control-Allow-Credentials": true,
+      },
 })
